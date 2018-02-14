1ee885f6511163bea708ba25ca7fe462f28ec4d5
Luis Borjas <luis@birchbox.com>
2018-02-14 16:39:39 -0500

    basic products spec working

--- spec/graphql/queries/product_query_spec.rb
+++ spec/graphql/queries/product_query_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 describe "Single Product Query" do
+  let!(:product){ FactoryBot.create(:product, title: "It's me, product!") }
   # from: http://graphql-ruby.org/schema/testing.html
   let(:context){ {} }
   let(:variables){ {} }
   let(:result){
     res = GraphqlMongoProductApiSchema.execute(
       query,
-      context: context
+      context: context,
       variables: variables
     )
     if res["errors"]
@@ -17,17 +18,28 @@ describe "Single Product Query" do
   }
 
   context "simple product representation" do
+    let(:variables){ {
+                       "productId" => product.id.to_s
+                     } }
+    # The query has to be named so we can declare, and later use, variables:
+    # http://graphql-ruby.org/queries/executing_queries
+    # http://graphql.org/learn/queries/#variables
     let(:query) {
-      <<-GRAPHQL
-        query{
+      "
+        query getProduct($productId: String!){
           product(id: $productId){
             id
             title
          }
         }
-      GRAPHQL
+      "
     }
-    let(:variables){ }
-    
+
+
+    it "returns the product's requested data" do
+      query_product = result["data"]["product"]
+      expect(query_product["id"]).to eq product.id.to_s
+      expect(query_product["title"]).to eq "It's me, product!"
+    end
   end
 end
